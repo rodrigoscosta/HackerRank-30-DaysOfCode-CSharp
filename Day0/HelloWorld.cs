0c5843b96b4ee9b62e1d742c01a4f9049169b71f
Rodrigo Santos Costa <AB1214210@br.atento.com>
2021-06-18 16:29:13 -0300

    Hello, World.

---
+++ Day0/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class HelloWorld
+{
+    static void Main(String[] args)
+    {      
+        String inputString; 
+        inputString = Console.ReadLine(); 
+        Console.WriteLine("Hello, World.");
+        Console.WriteLine(inputString);
+    }
+}
